32d53998a96d9042b45d0fad81892e399ca1c768
steve kieu <msh.computing@gmail.com>
2024-10-17 22:51:38 +1000

    Update

--- tloader/LoadTemplatesFromBinary.go
+++ tloader/LoadTemplatesFromBinary.go
@@ -1,98 +1,95 @@
 package tloader
 
 import (
-  "errors"
-  "log"
-  "html/template"
-  "sort"
-  "strings"
-)
-
-import (
-  "github.com/GeertJohan/go.rice"
+	"errors"
+	"github.com/GeertJohan/go.rice"
+	"html/template"
+	"log"
+	"sort"
+	"strings"
 )
 
 func LoadPathsFromBox(templatesBox *rice.Box, current *rice.File, parent string, paths []string) ([]string, error) {
 
-  current_stat, err := current.Stat()
-  if err != nil {
-    return nil, errors.New("Error: Could not stat current directory.")
-  }
+	current_stat, err := current.Stat()
+	if err != nil {
+		return nil, errors.New("Error: Could not stat current directory.")
+	}
 
-  basepath := current_stat.Name()
-  if len(parent) > 0 {
-    basepath = parent + "/" + basepath
-  }
+	basepath := current_stat.Name()
+	if len(parent) > 0 {
+		basepath = parent + "/" + basepath
+	}
 
-  infos, err := current.Readdir(0)
-  current.Close()
-  if err != nil {
-    return nil, errors.New("Error: Could not read directory names from rice box for templates.")
-  }
+	infos, err := current.Readdir(0)
+	current.Close()
+	if err != nil {
+		return nil, errors.New("Error: Could not read directory names from rice box for templates.")
+	}
 
-  for _, info := range infos {
-    if info.IsDir() {
-      next, err := templatesBox.Open(basepath+"/"+info.Name())
-      if err != nil {
-        return nil, err
-      }
-      paths, err = LoadPathsFromBox(templatesBox, next, basepath, paths)
-      if err != nil {
-        return nil, err
-      }
-    } else {
-      if basepath == "" || basepath == "/" {
-        paths = append(paths, info.Name())
-      } else {
-        paths = append(paths, basepath+"/"+info.Name())
-      }
-    }
-  }
+	for _, info := range infos {
+		if info.IsDir() {
+			next, err := templatesBox.Open(basepath + "/" + info.Name())
+			if err != nil {
+				return nil, err
+			}
+			paths, err = LoadPathsFromBox(templatesBox, next, basepath, paths)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			if basepath == "" || basepath == "/" {
+				paths = append(paths, info.Name())
+			} else {
+				paths = append(paths, basepath+"/"+info.Name())
+			}
+		}
+	}
 
-  return paths, nil
+	return paths, nil
 }
 
-func LoadTemplatesFromBinary(templatesBox *rice.Box, templateFunctions template.FuncMap, verbose bool) (* template.Template, error) {
+func LoadTemplatesFromBinary(templatesBox *rice.Box, templateFunctions template.FuncMap, verbose bool) (*template.Template, error) {
 
-  tmpl := template.New("blank.tpl.html").Funcs(templateFunctions)
+	tmpl := template.New("blank.tpl.html").Funcs(templateFunctions)
 
-  root, err := templatesBox.Open("/")
-  if err != nil {
-    return nil, errors.New("Error: Could not open root of rice box for templates.")
-  }
+	root, err := templatesBox.Open("/")
+	if err != nil {
+		return nil, errors.New("Error: Could not open root of rice box for templates.")
+	}
 
-  stat, err := root.Stat()
-  if err != nil {
-  	return nil, errors.New("Error: Could not stat root of rice box for templates.")
-  }
+	stat, err := root.Stat()
+	if err != nil {
+		return nil, errors.New("Error: Could not stat root of rice box for templates.")
+	}
 
 	if !stat.IsDir() {
 		return nil, errors.New("Error: Root of rice box for templates is not a directory.")
 	}
 
-  paths, err := LoadPathsFromBox(templatesBox, root, "", []string{})
-  if err != nil {
-    return nil, errors.New("Error: Could not load paths.")
-  }
-  sort.Strings(paths)
+	paths, err := LoadPathsFromBox(templatesBox, root, "", []string{})
+	if err != nil {
+		return nil, errors.New("Error: Could not load paths.")
+	}
+	sort.Strings(paths)
 
-  if verbose {
-    log.Println("Paths: "+strings.Join(paths, "; "))
-  }
+	if verbose {
+		log.Println("Paths: " + strings.Join(paths, "; "))
+	}
 
-  for _, path := range paths {
-    content, err := templatesBox.String(path)
-    if err != nil {
-      return nil, errors.New("Error: Could not read content from template at '"+path+"'.")
-    }
-    if verbose {
-      log.Println("Registering template for path '"+path+"'.")
-    }
-    tmpl, err = tmpl.New(path).Parse(content)
-    if err != nil {
-      return nil, errors.New("Error: Could not parse content from template at '"+path+"'.")
-    }
-  }
+	for _, path := range paths {
+		content, err := templatesBox.String(path)
+		if err != nil {
+			return nil, errors.New("Error: Could not read content from template at '" + path + "'.")
+		}
+		if verbose {
+			log.Println("Registering template for path '" + path + "'.")
+		}
+		tmpl, err = tmpl.New(path).Parse(content)
+		if err != nil {
+			return nil, errors.New("Error: Could not parse content from template at '" + path + "'.")
+		}
+	}
 
-  return tmpl, err
+	return tmpl, err
 }
--- tloader/LoadTemplatesFromPaths.go
+++ tloader/LoadTemplatesFromPaths.go
@@ -1,46 +1,44 @@
 package tloader
 
 import (
-  "errors"
-  "log"
-  "html/template"
-  "strings"
-  "io/ioutil"
+	"errors"
+	"html/template"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/ttacon/chalk"
 )
 
-import (
-  "github.com/ttacon/chalk"
-)
+func LoadTemplatesFromPaths(pathsToTemplates string, templateExtensions []string, templateFunctions template.FuncMap, verbose bool) (*template.Template, error) {
 
-func LoadTemplatesFromPaths(pathsToTemplates string, templateExtensions []string, templateFunctions template.FuncMap, verbose bool) (* template.Template, error) {
+	template_names := make([]string, 0)
+	template_paths := make([]string, 0)
+	for _, x := range strings.Split(pathsToTemplates, ":") {
+		collected_names, collected_paths, err := CollectTemplatePaths("", x, templateExtensions, true, []string{}, []string{})
+		if verbose {
+			log.Println(chalk.Green, "Names: ", collected_names, chalk.Reset)
+			log.Println(chalk.Green, "Paths: ", collected_paths, chalk.Reset)
+		}
+		if err != nil {
+			log.Println(chalk.Red, "Error: Could Not Collect templates files from ", x, chalk.Reset)
+			return nil, err
+		}
+		template_names = append(template_names, collected_names...)
+		template_paths = append(template_paths, collected_paths...)
+	}
 
-  template_names := make([]string, 0)
-  template_paths := make([]string, 0)
-  for _, x := range strings.Split(pathsToTemplates, ":") {
-    collected_names, collected_paths, err := CollectTemplatePaths("", x, templateExtensions, true, []string{}, []string{})
-    if verbose {
-      log.Println(chalk.Green, "Names: ", collected_names, chalk.Reset)
-      log.Println(chalk.Green, "Paths: ", collected_paths, chalk.Reset)
-    }
-    if err != nil {
-      log.Println(chalk.Red, "Error: Could Not Collect templates files from ", x, chalk.Reset)
-      return nil, err
-    }
-    template_names = append(template_names, collected_names...)
-    template_paths = append(template_paths, collected_paths...)
-  }
+	tmpl := template.New("blank.tpl.html").Funcs(templateFunctions)
+	for i := 0; i < len(template_names); i++ {
+		template_content, err := os.ReadFile(template_paths[i])
+		if err != nil {
+			return nil, errors.New("Error: Could not parse template at path " + template_paths[i] + " .")
+		}
+		tmpl, err = tmpl.New(template_names[i]).Parse(string(template_content))
+		if err != nil {
+			return tmpl, err
+		}
+	}
 
-  tmpl := template.New("blank.tpl.html").Funcs(templateFunctions)
-  for i := 0; i < len(template_names); i++ {
-    template_content, err := ioutil.ReadFile(template_paths[i])
-    if err != nil {
-      return nil, errors.New("Error: Could not parse template at path " +template_paths[i]+" .")
-    }
-    tmpl, err = tmpl.New(template_names[i]).Parse(string(template_content))
-    if err != nil {
-      return tmpl, err
-    }
-  }
-
-  return tmpl, nil
+	return tmpl, nil
 }
